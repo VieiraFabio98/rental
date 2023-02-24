099cca0d9d4186a3e09779ef18265ffd4e659302
root <root@DESKTOP-T0JJ47S.localdomain>
2023-02-24 10:08:14 -0300

    criando o usuário

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -3,7 +3,8 @@ import { ICategoriesRepository } from "../../modules/cars/repositories/ICategori
 import { CategoriesRepository } from "../../modules/cars/repositories/implementations/CategoriesRepository"
 import { ISpecificationsRepository } from "../../modules/cars/repositories/ISpecificationsRepository";
 import { SpecificationsRepository } from "../../modules/cars/repositories/implementations/SpecificationsRepository";
-
+import { IUsersRepository } from "../../modules/accounts/repositories/IUsersRepository";
+import { UsersRepository } from "../../modules/accounts/repositories/implementations/UsersRepository";
 // ICategoriesRepository
 container.registerSingleton<ICategoriesRepository>(
   "CategoriesRepository",
@@ -14,4 +15,9 @@ container.registerSingleton<ISpecificationsRepository>(
   "SpecificationsRepository",
   SpecificationsRepository
 );
+
+container.registerSingleton<IUsersRepository>(
+  "UsersRepository",
+  UsersRepository
+)
 
