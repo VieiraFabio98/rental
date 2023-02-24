90c673d7464046452e1f541af24a4caad1327a50
root <root@DESKTOP-T0JJ47S.localdomain>
2023-02-24 10:37:35 -0300

    colocando hash na senha e impedindo o cadastro de um mesmo email

--- src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
+++ src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
@@ -1,6 +1,7 @@
 import { IUsersRepository } from "../../repositories/IUsersRepository";
 import { inject, injectable} from "tsyringe"
 import { ICreateUserDTO } from "../../dtos/ICreateUserDTO";
+import { hash } from "bcrypt";
 
 @injectable()
 class CreateUserUseCase{
@@ -15,10 +16,19 @@ class CreateUserUseCase{
     email, 
     password, 
     driver_license }: ICreateUserDTO): Promise<void> {
+
+      const userAlreadyExists = await this.userRepository.findByEmail(email);
+
+      if(userAlreadyExists){
+        throw new Error("User already exists");
+      }
+    
+      const passwordHash = await hash(password, 8);
+
     await this.userRepository.create({
       name, 
       email, 
-      password, 
+      password: passwordHash, 
       driver_license});
   }
 }
