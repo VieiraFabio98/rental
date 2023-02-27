25c4f546ea911cb23ff2b02558d5e3b2756c47e8
root <root@DESKTOP-T0JJ47S.localdomain>
2023-02-27 14:00:29 -0300

    permitindo a exclusao de um avatar

--- src/modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarUseCase.ts
+++ src/modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarUseCase.ts
@@ -1,5 +1,6 @@
 import { inject, injectable } from "tsyringe";
 import { IUsersRepository } from "../../repositories/IUsersRepository";
+import { deleteFile } from "../../../../utils/file";
 
 
 interface IRequest {
@@ -18,6 +19,10 @@ class UpdateUserAvatarUseCase {
 
     async execute({user_id, avatarFile }: IRequest): Promise<void> { 
       const user = await this. usersRepository.findById(user_id);
+
+      if(user.avatar){
+        await deleteFile(`./tmp/avatar/${user.avatar}`);
+      }
 
       user.avatar = avatarFile;
 
