b233ff377119614bcf7addb49fa56efa93ad289f
root <root@DESKTOP-T0JJ47S.localdomain>
2023-03-02 13:01:53 -0300

    funcionando tudo depois do erro

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -1,33 +1,5 @@
-import "reflect-metadata";
-import express, { NextFunction, Request, Response } from 'express';
-import "express-async-errors";
-import { router } from "./routes";
-import swaggerUi from "swagger-ui-express";
-import swaggerfile from "../../../swagger.json";
-import "@shared/infra/database-typeorm";
-import "@shared/container";
-import { AppError  } from "@shared/errors/AppError";
+import { app } from "./app"
 
-const app = express();
+const port = 3333;
 
-app.use(express.json());
-
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerfile))
-
-app.use(router);
-
-app.use((err: Error, request: Request, response: Response, next: NextFunction) => {
-  if(err instanceof AppError) {
-    return response.status(err.statusCode).json({
-      message: err.message
-    });
-  }
-
-  return response.status(500).json({
-    status: "error",
-    message: `Internal server error - ${err.message}`,
-  });
- }
-);
-
-app.listen(3333, () => console.log("Server is listening")); 
+app.listen(port, () => console.log(`Server is running on port ${port}`))
