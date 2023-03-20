e3d3352395e9c2c8d44a2e83a0577229df3043a2
root <root@DESKTOP-T0JJ47S.localdomain>
2023-03-20 11:57:07 -0300

    reset e forget password

--- src/modules/accounts/useCases/sendForgotPasswordMail/SendForgotPasswordMailUseCase.ts
+++ src/modules/accounts/useCases/sendForgotPasswordMail/SendForgotPasswordMailUseCase.ts
@@ -5,6 +5,7 @@ import { IMailProvider } from "@shared/container/providers/MailProvider/IMailPro
 import { AppError } from "@shared/errors/AppError";
 import { inject, injectable } from "tsyringe";
 import { v4 as uuidV4} from "uuid";
+import { resolve } from "path"
 
 @injectable()
 class SendForgotPasswordMailUseCase {
@@ -23,6 +24,8 @@ class SendForgotPasswordMailUseCase {
   async execute(email: string): Promise<void>{
     const user = await this.userRepository.findByEmail(email);
 
+    const templatePath = resolve(__dirname, "..", "..", "views", "emails", "forgotpassword.hbs")
+
     if(!user){
       throw new AppError("User does not exists!");
     };
@@ -37,7 +40,17 @@ const expires_date = this.dateProvider.addHours(3);
       expires_date,
     });
 
-    await this.mailProvider.sendMail(email, "Recuperação de senha", `O link para o reset é ${token}`)
+    const variables = {
+      name: user.name,
+      link: `${process.env.FORGOT_MAIL_URL}${token}`,
+    }
+
+    await this.mailProvider.sendMail(
+      email, 
+      "Recuperação de senha", 
+      variables,
+      templatePath
+    )
 
   }
 }
--- src/shared/container/providers/DateProvider/implementations/DayJsDateProvider.ts
+++ src/shared/container/providers/DateProvider/implementations/DayJsDateProvider.ts
@@ -13,7 +13,7 @@ class DayJsDateProvider implements IDateProvider{
     
     return dayjs(end_date_utc).diff(start_date_utc, "hours");
   }
-
+  
   convertToUTC(date: Date): string {
     return dayjs(date)
     .utc()
@@ -24,14 +24,14 @@ class DayJsDateProvider implements IDateProvider{
   dateNow(): Date {
     return dayjs().toDate();
   }
-
+  
   compareInDays(start_date: Date, end_date: Date): number {
     const end_date_utc = this.convertToUTC(end_date);
     const start_date_utc = this.convertToUTC(start_date);
     
     return dayjs(end_date_utc).diff(start_date_utc, "days");
   };
-
+  
   addDays(days: number) {
     return dayjs().add(days, "days").toDate()
   };
@@ -40,6 +40,10 @@ class DayJsDateProvider implements IDateProvider{
     return dayjs().add(hours, "hour").toDate();
   }
   
+  compareIfBefore(start_date: Date, end_date: Date): boolean {
+    return dayjs(start_date).isBefore(end_date)
+  }
+
 }
 
 export { DayJsDateProvider };
